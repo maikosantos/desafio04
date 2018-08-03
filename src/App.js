04558503346a8ea8f81d04faa9f016fa41f4f1b8
Maiko <maikossmaster@gmail.com>
2018-08-03 16:22:36 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import "./styles/global";
+
+import "./config/reactotron";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Opa
-      </div>
-    );
-  }
+    render() {
+        return <div className="App">Opa</div>;
+    }
 }
 
 export default App;
